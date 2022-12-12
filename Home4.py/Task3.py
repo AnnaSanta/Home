9cc34598d57c7d62f05b48dd4996051e3d0808c4
Anna <Gera101184@gmail.com>
2022-12-13 01:48:01 +0300

    D

--- Home4.py/Task3.py
+++ Home4.py/Task3.py
@@ -5,8 +5,8 @@
 # - при [1, 1, 2, 3, 3, 4, 1, 5, 7, 8, 8, 7, 9]     ->        [2, 4, 5, 9]
 
 n = list(input('Задайте последовательность чисел: '))
-# n2 = list(set(n))
-n2 = list(dict.fromkeys(n))
-print(n2)
+n = list(dict.fromkeys(n))
+print(n)  
+
 
 
