11685ab2bf17428b18f447e3654ec1b183522fbd
Anna <Gera101184@gmail.com>
2022-12-18 23:14:46 +0300

    hhgh

--- Home5.py/Task1.py
+++ Home5.py/Task1.py
@@ -1,16 +1,14 @@
 # 1 Напишите программу, удаляющую из файла все слова, 
 # содержащие "абв"
 
-with open('Task.txt', 'a', encoding='utf-8') as f:
-    print('привет абв пока абвг\n', file=f)
+with open('Task1.txt', 'a', encoding='utf-8') as f:
+    print('привет абв пока абвг\n', file=f)    
 
-with open('Task.txt', 'w', encoding='utf-8') as f:  
-     p = ' '.join(list(filter(lambda x: not 'абв' in x, f.split())))
-     print(p)
+with open('Task1.txt', 'r', encoding='utf-8') as f:
+    print(f.read())  
 
-
-# with open('Task.txt', 'r', encoding='utf-8') as f:
-#    print(f.read())
-
-# with open('Task.txt', 'r', encoding='utf-8') as f:
-#     data = f.readlines() 
+with open('Task1.txt', 'w+', encoding='utf-8') as f:  
+    temp = f.read()  
+    z = [i for i in temp.split()]
+    p = ' '.join(list(filter(lambda x: not 'абв' in x, z)))
+    print(p)
