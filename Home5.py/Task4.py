55df5b0de9131baeb5c8c44d647f84a97e3c944d
Anna <Gera101184@gmail.com>
2022-12-20 01:08:57 +0300

    bjn

--- Home5.py/Task4.py
+++ Home5.py/Task4.py
@@ -4,3 +4,38 @@
 #  в отдельных текстовых файлах.
 # aaaasssdddwwwwwwwwwwwweeeffffff -> 4a3s3d9w3w3e6f
 # 4a3s3d9w3w3e6f-> aaaasssdddwwwwwwwwwwwweeeffffff 
+
+def encode(s):
+
+	encoding = "" # выходная строка
+
+	i = 0
+	while i < len(s):
+		count = 1
+		while i + 1 < len(s) and s[i] == s[i + 1]:
+			count = count + 1
+			i = i + 1
+
+		encoding += str(count) + s[i]
+		i = i + 1
+
+	return encoding
+
+
+if __name__ == '__main__':
+
+	s = 'aaaasssdddwwwwwwwwwwwweeeffffff'
+	print(encode(s))
+
+def rle_decode(data):
+    decode = ''
+    count = ''
+    for char in data:
+        if char.isdigit():
+           count += char 
+        else: 
+            decode += char * int(count)
+            count = ''
+    return decode   
+decoded_val = rle_decode('4a3s3d9w3w3e6f') 
+print(decoded_val)
